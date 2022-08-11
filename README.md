83a20d641ed72f12a5581a9e11ac875227e77779
Wompa-Stompa <chavothewonderdog@gmail.com>
2022-08-11 04:52:13 -0500

    Finally working

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# Professional README Generator Starter Code
+Professional ReadMe Generator 
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+Answer a series of questions to generate a ReadMe file for existing or new projects.
+
+The app should produce a ReadMe like the one that follows:
+
+# Project Name
+
+## Table of Contents
+* [ Project Links ](#Project-Links)
+* [ Screenshots-Demo ](#Screenshots)
+* [ Project Objective ](#Project-Objective)
+* [ User Story ](#User-Story)
+* [ Technologies ](#Technologies)
+* [ Installation ](#Installation)
+* [ Usage ](#Usage)
+* [ Credits and Reference ](#Credits-and-Reference)
+* [ Tests ](#Tests)
+* [ Author Contact ](#Author-Contact)
+* [ License ](#License)
---
+++ index.js
@@ -0,0 +1,80 @@
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process, if any: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is the purpose of the project?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Choose the appropriate license for this project: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who are the contributors to this project?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Is there a test included?"
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    ]);
+} 
+
+// Async function using util.promisify 
+  async function init() {
+    try {
+        // Ask user questions and generate responses
+        const answers = await promptUser();
+        const generateContent = generateMarkdown(answers);
+        // Write new README.md to dist directory
+        await writeFileAsync('./dist/README.md', generateContent);
+        console.log('✔️  Successfully wrote to README.md');
+    }   catch(err) {
+        console.log(err);
+    }
+  }
+  
+  init();  
